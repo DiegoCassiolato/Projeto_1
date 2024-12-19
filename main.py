c4550d09b99ac7f99b5888ddeb6145517c5e95d7
Diego Hermoso Cassiolato <diegohermoso88@gmail.com>
2024-12-18 22:33:18 -0300

    Commit configuração ambiente

---
+++ main.py
@@ -0,0 +1,18 @@
+print('jornada de dados')
+
+#variáveis
+idade = 34
+nome  = 'Diego'
+
+
+#Print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+
+#Listas
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse
